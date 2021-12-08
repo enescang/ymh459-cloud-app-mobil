3772795d187f377329b8a75dfb74feb01e70601d
kodcanlisi <developerindiee@gmail.com>
2021-12-09 00:13:49 +0300

    Clear files for upgrading react native version

--- src/screens/index.js
+++
@@ -1,7 +0,0 @@
-export * from './BeforeLogin/Login';
-export * from './BeforeLogin/SignUp';
-export * from './BeforeLogin/RememberPass';
-
-export * from './Home';
-export * from './Home/ScreenOne';
-export * from './Home/ScreenTwo';
