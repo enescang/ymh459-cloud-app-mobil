a358ac28ab6359f2f58779fe89d45bba5577fda8
kodcanlisi <developerindiee@gmail.com>
2021-12-13 19:19:05 +0300

    Create uuid when a file picked

--- src/Ops/FilePicker.js
+++ src/Ops/FilePicker.js
@@ -1,13 +1,17 @@
 import DocumentPicker from "react-native-document-picker";
+import uuid from 'react-native-uuid';
+
 
 const pickFile = async (type = DocumentPicker.types.allFiles) => {
     try {
-        const res = await DocumentPicker.pickSingle({type: type})
+        const res = await DocumentPicker.pickSingle({type: type});
+        const _id = uuid.v4();
         return {
             uri: res.uri,
             mime: res.type,
             name: res.name,
-            size: res.size
+            size: res.size,
+            _id: _id,
         }
     } catch (err) {
         console.log("FILE PICKER ERROR>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>", {err});
