08c2070c5cad0787f43485c631ea6dbb038fc7a3
kodcanlisi <developerindiee@gmail.com>
2021-12-30 14:35:43 +0300

    TR, EN, RU languages added

---
+++ src/config/languages.js
@@ -0,0 +1,149 @@
+const en = {
+    name:"Name",
+    email:"Email",
+    password:"Password",
+    main_page:"Main Page",
+    profile:"Profile",
+    joined_at:"Joined",
+    total_uploaded_files:"Total Uploaded Files",
+    total_device_files:"Total Device Files",
+    exit:"Exit",
+    select_file:"Select File",
+    uploaded:"Uploaded",
+    not_uploaded:"Not Uploaded",
+    about_file:"About File",
+    remove_file:"Remove File",
+    close:"Close",
+    upload_again:"Upload Again",
+    accept:"Accept",
+    copy:"Copy",
+    private_key_copied:"Private Key Copied. You will redirect to login page",
+    creating_private_key:"Please wait. Creating Private Key",
+    please_enter_valid_email:"Please enter valid email",
+    email_not_found:"Email not found",
+    password_wrong:"Wrong password",
+    unexpected_error:"Unexpected Error",
+    
+    otp_code_must_be_6_digits:"OTP code must be 6 digits",
+    otp_code_not_found:"OTP code not found",
+    otp_code_wrong:"Wrong OTP code",
+    otp_expired:"OTP expired",
+    please_enter_otp_code:"Please enter OTP code",
+    verify:"Verify",
+    cancel:"Cancel",
+
+
+    login:"Login",
+    register:"Register",
+    forget_password:"Forget Password?",
+
+    password_not_match:"Password not match",
+    signup_success_please_copy_private_key:"Signup success. Please copy your private key",
+
+
+    file_uploaded_successfully:"File uploaded successfully",
+    network_error:"Network error",
+}
+
+//TR
+const tr = {
+    name:"İsim",
+    email:"Email",
+    password:"Şifre",
+    main_page:"Ana Sayfa",
+    profile:"Profil",
+    joined_at:"Katıldı",
+    total_uploaded_files:"Toplam Yüklenen Dosya",
+    total_device_files:"Cihazdaki Dosyalar",
+    exit:"Çık",
+    select_file:"Dosya Seç",
+    uploaded:"Yüklendi",
+    not_uploaded:"Yüklenmeyen",
+    about_file:"Dosya Hakkında",
+    remove_file:"Dosyayı Kaldır",
+    close:"Kapat",
+    upload_again:"Yeniden Yükle",
+    accept:"Onayla",
+    copy:"Kopyala",
+    private_key_copied:"Özel anahtar kopyalandı. Ana sayfaya yönlendiriliyorsunuz",
+    creating_private_key:"Lütfen bekleyin. Özel anahtar oluşturuluyor",
+    please_enter_valid_email:"Lütfen geçerli mail giriniz",
+    email_not_found:"Email Bulunamadı",
+    password_wrong:"Şifre Yanlış",
+    unexpected_error:"Beklenmedik bir hata oluştu",
+    
+    otp_code_must_be_6_digits:"OTP kodu 6 karakter olmalı",
+    otp_code_not_found:"OTP kodu bulunamadı",
+    otp_code_wrong:"Geçersiz OTP kodu",
+    otp_expired:"OTP kodunun süresi doldu",
+    please_enter_otp_code:"Lütfen OTP kodunu giriniz",
+    verify:"Onayla",
+    cancel:"İptal",
+
+
+    login:"Giriş",
+    register:"Kayıt Ol",
+    forget_password:"Şifremi unuttum",
+
+    password_not_match:"Şifreniz uyuşmuyor",
+    signup_success_please_copy_private_key:"Giriş Başarılı. Lütfen özel anahtarınızı kopyalayın",
+
+
+    file_uploaded_successfully:"Dosya yükleme başarılı",
+    network_error:"Bağlantı Hatası",
+}
+
+const ru = {
+    name:"Имя",
+    email:"Е-мейл",
+    password:"Пароль",
+    main_page:"Главная",
+    profile:"Профиль",
+    joined_at:"Присоединился",
+    total_uploaded_files:"Всего загруженных файлов",
+    total_device_files:"Всего файлов устройства",
+    exit:"Выход",
+    select_file:"Выбрать файл",
+    uploaded:"Загружено",
+    not_uploaded:"Не загружено",
+    about_file:"О файле",
+    remove_file:"Удалить файл",
+    close:"Закрывать",
+    upload_again:"Загрузить еще раз",
+    accept:"Принимать",
+    copy:"Копировать",
+    private_key_copied:"Закрытый ключ скопирован. Вы будете перенаправлены на страницу входа",
+    creating_private_key:"Пожалуйста подождите. Создание закрытого ключа",
+    please_enter_valid_email:"Пожалуйста, введите действующий адрес электронной почты",
+    email_not_found:"Электронная почта не найдена",
+    password_wrong:"Неправильный пароль",
+    unexpected_error:"Неожиданная ошибка",
+    
+    otp_code_must_be_6_digits:"Код OTP должен состоять из 6 цифр.",
+    otp_code_not_found:"Код OTP не найден",
+    otp_code_wrong:"Неверный код OTP",
+    otp_expired:"Срок действия OTP пароля истек",
+    please_enter_otp_code:"Пожалуйста, введите OTP-код",
+    verify:"Проверять",
+    cancel:"Отмена",
+
+
+    login:"Авторизоваться",
+    register:"регистр",
+    forget_password:"Забыть пароль?",
+
+    password_not_match:"Пароль не совпадает",
+    signup_success_please_copy_private_key:"Успешная регистрация. Скопируйте свой закрытый ключ",
+
+
+    file_uploaded_successfully:"Файл успешно загружен",
+    network_error:"Сетевая ошибка",
+}
+
+const LANGUAGES = {
+    en,
+    tr,
+    ru,
+}
+
+export {LANGUAGES}
